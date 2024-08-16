84b9dee6ea83280964bc87f08ef4a4dfa71191f7
carlobosio <carlobosioit@gmail.com>
2024-08-15 18:12:36 -0700

    added logits print

--- funsearch/programs_database.py
+++ funsearch/programs_database.py
@@ -37,6 +37,7 @@ def _softmax(logits: np.ndarray, temperature: float) -> np.ndarray:
   """Returns the tempered softmax of 1D finite `logits`."""
   if not np.all(np.isfinite(logits)):
     non_finites = set(logits[~np.isfinite(logits)])
+    print("logits: ", logits)
     raise ValueError(f'`logits` contains non-finite value(s): {non_finites}')
   if not np.issubdtype(logits.dtype, np.floating):
     logits = np.array(logits, dtype=np.float32)
