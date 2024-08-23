0f383d4e29edb5f8996033884af1ab745050c230
carlobosio <carlobosioit@gmail.com>
2024-08-22 17:55:10 -0700

    modified to produce plots

--- tests/inv_pendulum_test.py
+++ tests/inv_pendulum_test.py
@@ -6,13 +6,14 @@ Try to make the code short and be creative with the method you use.
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-def evaluate(init_angle) -> float:
+def test(init_angle):
   """Returns the negative log rmse score for a policy."""
-  rmse_value = solve(init_angle)
+  rmse_value, time, input, state = solve(init_angle)
   # print(f"[run] output rmse: {rmse_value}")
   if np.isfinite(rmse_value):
-    return float(-np.log(rmse_value))
+    return float(-np.log(rmse_value)), time, input, state
   else:
     print(f"[run] output rmse is not finite: {rmse_value}")
     return -100.0
@@ -26,12 +27,18 @@ def solve(init_angle) -> float:
   state = initial_state.copy()
   rmse_sum = 0.0
 
-  for _ in range(horizon_length):
+  control_input_seq = np.zeros(horizon_length)
+  state_seq = np.zeros((horizon_length, 2))
+  state_seq[0] = state.copy()
+  time_horizon = sampling_time*np.arange(horizon_length)
+  for t in range(horizon_length):
     control_input = policy(state)
+    control_input_seq[t] = control_input
     state = simulate(state, control_input, sampling_time)
+    state_seq[t] = state.copy()
     rmse_sum += np.linalg.norm(state)
-  
-  return rmse_sum / horizon_length
+
+  return rmse_sum / horizon_length, time_horizon, control_input_seq, state_seq
 
 def simulate(state: np.ndarray, control_input: float, sampling_time: float) -> np.ndarray:
   """Simulates a step.
@@ -48,13 +55,21 @@ def policy(state: np.ndarray) -> float:
   """Returns a control input. state is a 2D array contaning x and x_dot.
   The function is going to return a float input value.
   """
-  x, x_dot = state
-  k_p = -36
-  k_d = -30
-  u = k_p * x + k_d * x_dot
-
-  return u
+  x = state
+  x1, x2 = x[0], x[1]
+  return x1**2 + x2**2 - 2*x1 - 2*x2
 
 if __name__ == "__main__":
-  score = evaluate(0.6)
+  score, time, input, state = test(-1.0)
   print(f"Score: {score}")
+  plt.figure()
+  plt.subplot(2, 1, 1)
+  plt.plot(time, input)
+  plt.xlabel("Time")
+  plt.ylabel("Control Input")
+  plt.subplot(2, 1, 2)
+  plt.plot(time, state[:, 0], label="Ang")
+  plt.plot(time, state[:, 1], label="Ang Vel")
+  plt.xlabel("Time")
+  plt.legend()
+  plt.show()
