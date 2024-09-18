5f676dd10b62f0ac398631020b895644611ce3e3
carlobosio <carlobosioit@gmail.com>
2024-09-18 11:10:23 -0700

    fixed time step count and added multiple runs

--- examples/dm_control_spec.py
+++ examples/dm_control_spec.py
@@ -12,24 +12,27 @@ from dm_control import suite
 
 
 @funsearch.run
-def solve(param) -> float:
+def solve(num_runs) -> float:
   """Returns the reward for a heuristic.
   """
   env = suite.load(domain_name="pendulum", task_name="swingup")
-  time_step = env.reset()
-  total_reward = 0.0
-  for _ in range(1000):
-    action = heuristic(time_step.step_type.value, time_step.observation, env.action_spec().shape)
-    np.clip(action, env.action_spec().minimum, env.action_spec().maximum, out=action)
-    time_step = env.step(action)
-    total_reward += time_step.reward
-  return total_reward
+  avg_reward = 0.0
+  for _ in range(num_runs):
+    time_step = env.reset()
+    total_reward = 0.0
+    for _ in range(1000):
+      action = heuristic(env._step_count, time_step.observation, env.action_spec().shape)
+      np.clip(action, env.action_spec().minimum, env.action_spec().maximum, out=action)
+      time_step = env.step(action)
+      total_reward += time_step.reward
+    avg_reward += total_reward
+  return avg_reward / num_runs
+  # return total_reward
 
 @funsearch.evolve
 def heuristic(t: int, obs: np.ndarray, output_shape: tuple) -> float:
   """Returns an action of shape output_shape.
   t is a time counter.
   """
-
   action = np.random.uniform(-1, 1, output_shape)
   return action
