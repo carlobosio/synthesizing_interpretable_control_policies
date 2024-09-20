4d272423233a876aa476187bf5b7cc0f20e02b64
carlobosio <carlobosioit@gmail.com>
2024-09-19 19:39:11 -0700

    added initialization and modified reward

--- examples/dm_control_spec.py
+++ examples/dm_control_spec.py
@@ -19,6 +19,7 @@ def solve(num_runs) -> float:
   avg_reward = 0.0
   for _ in range(num_runs):
     time_step = env.reset()
+    initialize_env(env)
     total_reward = 0.0
     sum_diff = 0.0
     for _ in range(1000):
@@ -26,23 +27,34 @@ def solve(num_runs) -> float:
       np.clip(ref, -np.pi, np.pi, out=ref)
       cos_ref = np.cos(ref)
       sin_ref = np.sin(ref)
-      cos_obs = time_step.observation['orientation'][0]
-      sin_obs = -time_step.observation['orientation'][1]
-      cos_diff = cos_ref*cos_obs + sin_ref*sin_obs
-      sin_diff = sin_ref*cos_obs - cos_ref*sin_obs
+      cos_theta = time_step.observation['orientation'][0]
+      sin_theta = -time_step.observation['orientation'][1]
+      theta = np.arctan2(sin_theta, cos_theta)
+      cos_diff = cos_ref*cos_theta + sin_ref*sin_theta
+      sin_diff = sin_ref*cos_theta - cos_ref*sin_theta
       diff = np.arctan2(sin_diff, cos_diff)
-      action = 3/np.pi * diff - 2*time_step.observation['velocity'] + 0.1*sum_diff
+      action = 3/np.pi * diff + 0.1*sum_diff - 0.1*time_step.observation['velocity']
       np.clip(action, env.action_spec().minimum, env.action_spec().maximum, out=action)
       time_step = env.step(action)
-      total_reward += time_step.reward
+      # total_reward += time_step.reward
+      total_reward += 1.0 - np.abs(theta)/np.pi
     avg_reward += total_reward
   return avg_reward / num_runs
   # return total_reward
 
+def initialize_env(env):
+  env.physics.named.data.qpos['hinge'][0] = np.pi
+  env.physics.named.data.qvel['hinge'][0] = 0.0
+  env.physics.named.data.qacc['hinge'][0] = 0.0
+  env.physics.named.data.qacc_smooth['hinge'][0] = 0.0
+  env.physics.named.data.qacc_warmstart['hinge'][0] = 0.0
+  env.physics.named.data.actuator_moment['torque'][0] = 0.0
+  env.physics.named.data.qfrc_bias['hinge'][0] = 0.0
+
 @funsearch.evolve
-def heuristic(t: int, obs: np.ndarray, output_shape: tuple) -> float:
+def heuristic(t: int, obs: np.ndarray) -> float:
   """Returns an action reference between -pi and pi of shape output_shape.
-  t is a time counter. obs is the observation.
+  t is a time counter. obs is the observation [zz, xz, vel].
   """
-  ref = np.random.uniform(-1, 1, output_shape)
+  ref = 0.0
   return ref
