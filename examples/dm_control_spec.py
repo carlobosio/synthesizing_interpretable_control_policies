1074334fce440204a8b4935c9c6c40c277b7f80e
carlobosio <carlobosioit@gmail.com>
2024-09-20 18:25:02 -0700

    added concatenate_obs function

--- examples/dm_control_spec.py
+++ examples/dm_control_spec.py
@@ -16,12 +16,13 @@ def solve(num_runs) -> float:
   """Returns the reward for a heuristic.
   """
   env = suite.load(domain_name="pendulum", task_name="swingup")
+  obs_spec = env.observation_spec()
   avg_reward = 0.0
   for _ in range(num_runs):
     time_step = env.reset()
     initialize_env(env)
     total_reward = 0.0
-    obs = concatenate_obs(time_step)
+    obs = concatenate_obs(time_step, obs_spec)
     sum_diff = 0.0
     for _ in range(1000):
       # ref = heuristic(env._step_count, time_step.observation)
@@ -43,12 +44,12 @@ def solve(num_runs) -> float:
       total_reward += 1.0 - np.abs(theta)/np.pi
       if np.abs(theta) < 0.5:
         total_reward += 1.0
-      obs = concatenate_obs(time_step)
+      obs = concatenate_obs(time_step, obs_spec)
     avg_reward += total_reward
   return avg_reward / num_runs
   # return total_reward
 
-def concatenate_obs(time_step):
+def concatenate_obs(time_step, obs_spec):
   return np.concatenate([time_step.observation[k].ravel() for k in obs_spec])
 
 def initialize_env(env):
