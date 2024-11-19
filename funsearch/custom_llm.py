11d1ee85796bea06ce9852a59e441c70ee60f250
carlobosio <carlobosioit@gmail.com>
2024-11-19 15:03:00 -0800

    test

--- funsearch/custom_llm.py
+++ funsearch/custom_llm.py
@@ -71,13 +71,14 @@ class CustomLLM(torch.nn.Module):
 if __name__ == "__main__":
     quantization_config = BitsAndBytesConfig(load_in_8bit=True)
     llm = CustomLLM(samples_per_prompt=1, device="cuda:1", quantization_config=quantization_config)
-    prompt = ("You are an exceptionally intelligent coding assistant" 
-              "that consistently delivers accurate and reliable responses to user instructions.\n\n"
-              "### Instruction Complete the function.\n"
-              "def fibonacci(n):\n"
-              "# fill here\n"
-              "return out\n"
-              "### Response")
+    # prompt = ("You are an exceptionally intelligent coding assistant" 
+    #           "that consistently delivers accurate and reliable responses to user instructions.\n\n"
+    #           "### Instruction Complete the function.\n"
+    #           "def fibonacci(n):\n"
+    #           "# fill here\n"
+    #           "return out\n"
+    #           "### Response")
+    prompt = "def sum_first_n(n):\n"
              
     samples = llm.draw_samples(prompt=prompt)
     for i, sample in enumerate(samples, 1):
